14b53dbaf5975eb1e2a860fa417c6b74fe6581e8
craigryy <harriajames97@gmail.com>
2023-06-13 16:51:17 +0100

    made migration

--- notes/settings.py
+++ notes/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
+    'notespad',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- notespad/models.py
+++ notespad/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+
+    ''' Note Folder Model defined here'''
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='notes')
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    reated_by = models.CharField(max_length=100, blank=True)
+
+    def __unicode__(self):
+         return "Bucketlist: %s user: %s" % (self.name, self.user)
+
+    class Meta:
+         ordering = ('-date_created',)
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+
+    '''Pages  Model defined here'''
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note_folder = models.ForeignKey(Note, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content =  models.TextField()
+
+
+    def __str__(self):
+        return self.title
